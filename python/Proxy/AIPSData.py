6b59b555bafce9d8f1a5cb252419d02ddb78fd73
Mark Kettenis <kettenis@jive.eu>
2005-04-05 14:33:29 +0200

    Better error handling.

--- python/Proxy/AIPSData.py
+++ python/Proxy/AIPSData.py
@@ -17,52 +17,65 @@ class AIPSData:
                                    True, False, self.err)
 
     def exists(self, desc):
-        try:
-            self._init(desc)
-        except:
+        self._init(desc)
+        if OErr.PIsErr(self.err):
+            OErr.PClear(self.err)
             return False
         return True
 
+    def _verify(self, desc):
+        data = self._init(desc)
+        if OErr.PIsErr(self.err):
+            OErr.printErrMsg(self.err, "AIPSData._verify")
+            raise RuntimeError, "Cannot open data set %s" % desc['name']
+        return data
+
     def verify(self, desc):
-        self._init(desc)
+        data = self._verify(desc)
         return True                # Return something other than None.
 
     def header(self, desc):
-        data = self._init(desc)
+        data = self._verify(desc)
         return data.Desc.Dict
 
     def tables(self, desc):
-        data = self._init(desc)
+        data = self._verify(desc)
         return TableList.PGetList(data.TableList, self.err)
 
     def table_highver(self, desc, type):
-        data = self._init(desc)
+        data = self._verify(desc)
         return TableList.PGetHigh(data.TableList, type)
 
     def zap(self, desc):
-        data = self._init(desc)
+        data = self._verify(desc)
         data.Zap(self.err)
         return True                # Return something other than None.
 
     def header_table(self, desc, type, version):
-        data = self._init(desc)
+        data = self._verify(desc)
         table = data.NewTable(1, type, version, self.err)
         if OErr.PIsErr(self.err):
             OErr.printErrMsg(self.err, "AIPSData.header_table")
+            msg = "Cannot open %s table version %d", (type, version)
+            raise RuntimeError, msg
         return table.Desc.Dict
 
     def getrow_table(self, desc, type, version, rowno):
-        data = self._init(desc)
+        data = self._verify(desc)
         table = data.NewTable(1, type, version, self.err)
         if OErr.PIsErr(self.err):
             OErr.printErrMsg(self.err, "AIPSData.getrow_table")
+            msg = "Cannot open %s table version %d", (type, version)
+            raise RuntimeError, msg
         return table.ReadRow(rowno, self.err)
 
     def zap_table(self, desc, type, version):
-        data = self._init(desc)
+        data = self._verify(desc)
         data.ZapTable(type, version, self.err)
         if OErr.PIsErr(self.err):
             OErr.printErrMsg(self.err, "AIPSData.zap_table")
+            msg = "Cannot open %s table version %d", (type, version)
+            raise RuntimeError, msg
         data.UpdateTables(self.err)
         return True                # Return something other than None.
 
@@ -74,8 +87,6 @@ class AIPSImage(AIPSData):
         image = Image.newPAImage(desc['name'], desc['name'], desc['klass'],
                                  desc['disk'], desc['seq'], True, self.err)
         OSystem.PSetAIPSuser(userno)
-        if OErr.PIsErr(self.err):
-            OErr.printErrMsg(self.err, "AIPSImage._init")
         return image
 
 
@@ -86,6 +97,4 @@ class AIPSUVData(AIPSData):
         uvdata = UV.newPAUV(desc['name'], desc['name'], desc['klass'],
                             desc['disk'], desc['seq'], True, self.err)
         OSystem.PSetAIPSuser(userno)
-        if OErr.PIsErr(self.err):
-            OErr.printErrMsg(self.err, "AIPSUVData._init")
         return uvdata
