de2a7a4362221dd657809b20145b868f4e88be1f
Mark Kettenis <kettenis@jive.eu>
2005-04-08 12:14:28 +0200

    Adapt to recent changes.

--- test/mandl.py
+++ test/mandl.py
@@ -3,22 +3,14 @@ from AIPSTask import AIPSTask
 from AIPSData import AIPSImage
 
 AIPS.userno = 3601
-AIPSTask.version = '31DEC05'
 
-name = 'MANDELBROT'
-klass = 'MANDL'
-disk = 3
-seq = 1
+image = AIPSImage('MANDELBROT', 'MANDL', 1, 1)
 
 mandl = AIPSTask('mandl')
-mandl.outname = name
-mandl.outclass = klass
-mandl.outdisk = disk
-mandl.outseq = seq
-mandl.imsize = [ 512.0, 512.0 ]
+mandl.outdata = image
+mandl.imsize[1:] = [ 512, 512 ]
 mandl.go()
 
-image = AIPSImage(name, klass, disk, seq)
 try:
     header = image.header()
     print 'Dimension: %dx%d' % (header['inaxes'][0], header['inaxes'][1])
