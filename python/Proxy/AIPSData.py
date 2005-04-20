19bc61a0fd1d76bc5b8736e16a5dc5501b05b1b4
Mark Kettenis <kettenis@jive.eu>
2005-04-20 15:06:44 +0200

    Adjust for the fact that Obit throws proper exceptions now.

--- python/Proxy/AIPSData.py
+++ python/Proxy/AIPSData.py
@@ -17,16 +17,19 @@ class AIPSData:
                                    True, False, self.err)
 
     def exists(self, desc):
-        self._init(desc)
-        if OErr.PIsErr(self.err):
-            OErr.PClear(self.err)
+        try:
+            self._init(desc)
+        except OErr.OErr, err:
+            OErr.PClear(err)
             return False
         return True
 
     def _verify(self, desc):
-        data = self._init(desc)
-        if OErr.PIsErr(self.err):
-            OErr.printErrMsg(self.err, "AIPSData._verify")
+        try:
+            data = self._init(desc)
+        except OErr.OErr, err:
+            print err
+            #OErr.printErrMsg(err, "AIPSData._verify")
             raise RuntimeError, "Cannot open data set %s" % desc['name']
         return data
 
@@ -36,7 +39,9 @@ class AIPSData:
 
     def header(self, desc):
         data = self._verify(desc)
-        return data.Desc.Dict
+        header = data.Desc.Dict
+        data.Close(self.err)
+        return header
 
     def tables(self, desc):
         data = self._verify(desc)
@@ -44,7 +49,7 @@ class AIPSData:
 
     def table_highver(self, desc, type):
         data = self._verify(desc)
-        return TableList.PGetHigh(data.TableList, type)
+        return  TableList.PGetHigh(data.TableList, type)
 
     def zap(self, desc):
         data = self._verify(desc)
@@ -53,30 +58,33 @@ class AIPSData:
 
     def header_table(self, desc, type, version):
         data = self._verify(desc)
-        table = data.NewTable(1, type, version, self.err)
-        if OErr.PIsErr(self.err):
-            OErr.printErrMsg(self.err, "AIPSData.header_table")
+        try:
+            table = data.NewTable(1, type, version, self.err)
+        except OErr.OErr, err:
+            OErr.printErrMsg(err, "AIPSData.header_table")
             msg = "Cannot open %s table version %d", (type, version)
             raise RuntimeError, msg
         return table.Desc.Dict
 
     def getrow_table(self, desc, type, version, rowno):
         data = self._verify(desc)
-        table = data.NewTable(1, type, version, self.err)
-        if OErr.PIsErr(self.err):
-            OErr.printErrMsg(self.err, "AIPSData.getrow_table")
+        try:
+            table = data.NewTable(1, type, version, self.err)
+        except OErr.OErr, err:
+            OErr.printErrMsg(err, "AIPSData.getrow_table")
             msg = "Cannot open %s table version %d", (type, version)
             raise RuntimeError, msg
         return table.ReadRow(rowno, self.err)
 
     def zap_table(self, desc, type, version):
         data = self._verify(desc)
-        data.ZapTable(type, version, self.err)
-        if OErr.PIsErr(self.err):
-            OErr.printErrMsg(self.err, "AIPSData.zap_table")
-            msg = "Cannot open %s table version %d", (type, version)
+        try:
+            data.ZapTable(type, version, self.err)
+            data.UpdateTables(self.err)
+        except OErr.OErr, err:
+            OErr.printErrMsg(err, "AIPSData.zap_table")
+            msg = "Cannot zap %s table version %d", (type, version)
             raise RuntimeError, msg
-        data.UpdateTables(self.err)
         return True                # Return something other than None.
 
 
