289b7d39c7bec96975ab12b7fe87b24e4c79383e
Mark Kettenis <kettenis@jive.eu>
2005-09-27 14:17:36 +0200

    New test.

---
+++ test/fitld2.py
@@ -0,0 +1,36 @@
+from AIPS import AIPS
+from AIPSTask import AIPSTask
+from AIPSData import AIPSImage
+
+import os
+import urllib
+
+AIPS.userno = 1999
+
+# Download a smallish FITS file from the EVN archive.
+url = 'http://archive.jive.nl/exp/N05L1_050301/pipe/n05l1_4C39.25_ICLN.FITS'
+file = '/tmp/' + os.path.basename(url)
+if not os.path.isfile(file):
+    urllib.urlretrieve(url, file)
+assert(os.path.isfile(file))
+
+name = os.path.basename(url).split('_')[1].upper()
+image = AIPSImage(name, 'ICLN', 1, 1)
+if image.exists():
+    image.zap()
+
+fitld = AIPSTask('fitld')
+fitld.infile = file
+fitld.outdata = image
+#fitld.msgkill = 2
+fitld.go()
+
+try:
+    print 'Stokes:', image.stokes
+    assert(len(image.stokes) == 1)
+
+    assert(image.header.date_obs == '2005-03-01')
+    assert(image.header.telescop.rstrip() == 'EVN')
+
+finally:
+    image.zap()
