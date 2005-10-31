e0e4ae5d2bb92fa5d92a5c43c2b4c26e0145bb30
Mark Kettenis <kettenis@jive.eu>
2005-10-31 13:40:46 +0100

    Remove ehex; moved to AIPSUtil.

--- python/Proxy/AIPS.py
+++ python/Proxy/AIPS.py
@@ -21,22 +21,4 @@ class AIPS:
     # Default AIPS systen format revision.
     revision = 'D'
 
-
-def ehex(n, width, padding=None):
-    ehex_digits = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    result = ''
-
-    while n > 0:
-        result = ehex_digits[n % len(ehex_digits)] + result
-        n /= len(ehex_digits)
-        width -= 1
-        continue
-
-    # Pad if requested to do so.
-    if padding != None:
-        while width > 0:
-            result = str(padding) + result
-            width -= 1
-            continue
-
-    return result
+    pass                                # class AIPS
