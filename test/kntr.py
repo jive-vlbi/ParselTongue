b56176d769eeb2475b69f37e3296234932ce6b32
Mark Kettenis <kettenis@jive.eu>
2005-11-02 14:31:03 +0100

    New test that uses the AIPS TV.

---
+++ test/kntr.py
@@ -0,0 +1,45 @@
+import AIPS
+from AIPSTask import AIPSTask
+from AIPSData import AIPSImage
+from AIPSTV import AIPSTV
+
+import os
+import urllib
+
+AIPS.userno = 1999
+
+# Download a smallish FITS file from the EVN archive.
+url = 'http://archive.jive.nl/exp/N05L1_050301/pipe/n05l1_4C39.25_ICLN.FITS'
+file = '/tmp/' + os.path.basename(url)
+if not os.path.isfile(file):
+    urllib.urlretrieve(url, file)
+assert(os.path.isfile(file))
+
+name = os.path.basename(url).split('_')[1].upper()
+image = AIPSImage(name, 'ICLN', 1, 1)
+if image.exists():
+    image.zap()
+
+fitld = AIPSTask('fitld')
+fitld.infile = file
+fitld.outdata = image
+fitld.msgkill = 2
+fitld.go()
+
+try:
+    pid = os.spawnv(os.P_NOWAIT, "../bin/TVServer", ['TVServer'])
+
+    kntr = AIPSTask('kntr')
+    kntr.indata = image
+    kntr.docont = 0
+    kntr.dovect = 0
+    kntr.dotv = 1
+    kntr.go()
+
+    tv = AIPSTV()
+    tv.kill()
+
+    os.waitpid(pid, 0)
+
+finally:
+    image.zap()
