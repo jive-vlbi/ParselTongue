3ab00b69c432e46b8098c557b4fa46ba69354870
Mark Kettenis <kettenis@jive.eu>
2005-11-02 16:03:58 +0100

    Add AIPSTV.start().

--- python/AIPSTV.py
+++ python/AIPSTV.py
@@ -22,17 +22,20 @@ to control the AIPS TV from Python.
 """
 
 # Generic Python stuff.
-import socket, struct
+import os, socket, struct, time
 
 
 class AIPSTV(object):
     def __init__(self, host='localhost'):
         self.host = host
         self.port = socket.getservbyname('sssin', 'tcp')
+        self._lock_pid = 0
+        self._server_pid = 0
         return
 
     def _send(self, opcode, dat1=0, dat2=0, dat3=0, dat4=0):
-        """Send command to the TV server."""
+        """Send command to the AIPS TV server."""
+
         s = struct.pack('!6h', opcode, dat1, dat2, dat3, dat4, 0)
         self._socket.send(s)
         if opcode == 12:
@@ -45,30 +48,68 @@ class AIPSTV(object):
         return
 
     def _open(self):
-        """Open connection to the TV server."""
+        """Open connection to the AIPS TV server."""
+
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._socket.connect((self.host, self.port))
         self._send(11)
         return
 
     def _close(self):
-        """Close connection to the TV server."""
+        """Close connection to the AIPS TV server."""
+
         self._socket.send(struct.pack('!6h', 12, 0, 0, 0, 0, 0))
         self._socket.close()
         return
 
+    def start(self):
+        """Start the AIPS TV server."""
+
+        # Check if we already started the AIPS TV.
+        if self._lock_pid and self._server_pid:
+            raise RuntimeError, "the AIPS TV has already been started"
+
+        # Create an environment for the AIPS TV processes.
+        env = os.environ.copy()
+        env['TVDEV'] = 'TVDEV01'
+        env['TVDEV01'] = 'sssin:localhost'
+
+        # Start the AIPS TV lock daemon.
+        file = env['LOAD'] + '/TVSERV.EXE'
+        self._lock_pid = os.spawnve(os.P_NOWAIT, file, ['TVSERVER'], env)
+
+        # Start the AIPS TV server.
+        file = env['LOAD'] + '/XAS'
+        self._server_pid = os.spawnve(os.P_NOWAIT, file, ['XAS'], env)
+
+        # Wait until the TV server has been started.
+        time.sleep(2)
+        return
+
     def clear(self):
-        """Init the TV server."""
+        """Init the AIPS TV server."""
+
         self._open()
         self._send(15)
         self._close()
         return
 
     def kill(self):
-        """Close down the TV server."""
+        """Close down the AIPS TV server."""
+
         self._open()
         self._socket.send(struct.pack('!6h', 18, 0, 0, 0, 0, 0))
         self._socket.close()
+
+        # Kill of the zombies.
+        if self._lock_pid:
+            os.waitpid(self._lock_pid, 0)
+            self._lock_pid = 0
+            pass
+        if self._server_pid:
+            os.waitpid(self._server_pid, 0)
+            self._server_pid = 0
+            pass
         return
 
     pass                                # Class AIPSTV
