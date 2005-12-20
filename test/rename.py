1f3c5f871d64650678d754e4ebe1980ed95b627b
Mark Kettenis <kettenis@jive.eu>
2005-12-20 15:32:39 +0100

    Test for renaming AIPS data.

---
+++ test/rename.py
@@ -0,0 +1,35 @@
+from AIPS import AIPS
+from AIPSTask import AIPSTask
+from AIPSData import AIPSImage
+
+AIPS.userno = 1999
+
+image = AIPSImage('MANDELBROT', 'MANDL', 1, 1)
+if image.exists():
+    image.zap()
+
+mandl = AIPSTask('mandl')
+mandl.outdata = image
+mandl.imsize[1:] = [ 512, 512 ]
+mandl.go()
+
+try:
+    image.rename('APPLE')
+    assert(image.exists())
+    assert(AIPSImage('APPLE', 'MANDL', 1, 1).exists())
+    assert(not AIPSImage('MANDELBROT', 'MANDL', 1, 1).exists())
+    image.rename(seq = 8)
+    assert(image.exists())
+    assert(image.seq == 8)
+    image.rename(klass='APPLE', seq=99)
+    assert(image.exists())
+    image.rename('MANDLEBROT', seq=1)
+    assert(image.exists())
+    try:
+        image.rename('MANDLEBROT', seq=1)
+    except:
+        pass
+    else:
+        raise AssertionError, "Rename unexpectedly succeeded"
+finally:
+    image.zap()
