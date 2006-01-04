aeca3ee03cb5831f0c781bbcb65e41ba2e681792
Mark Kettenis <kettenis@jive.eu>
2006-01-04 14:37:56 +0100

    New test for history functionality.

---
+++ test/history.py
@@ -0,0 +1,44 @@
+from AIPS import AIPS
+from AIPSTask import AIPSTask
+from AIPSData import AIPSUVData
+
+import os
+import urllib
+
+AIPS.userno = 1999
+
+# Download a smallish FITS file from the EVN archive.
+url = 'http://archive.jive.nl/exp/N03L1_030225/fits/n03l1_1_1.IDI1'
+file = '/tmp/' + os.path.basename(url)
+if not os.path.isfile(file):
+    urllib.urlretrieve(url, file)
+assert(os.path.isfile(file))
+
+name = os.path.basename(url).split('_')[0].upper()
+uvdata = AIPSUVData(name, 'UVDATA', 1, 1)
+if uvdata.exists():
+    uvdata.zap()
+    pass
+
+assert(not uvdata.exists())
+
+fitld = AIPSTask('fitld')
+fitld.infile = file
+fitld.outdata = uvdata
+fitld.msgkill = 2
+fitld.go()
+
+assert(uvdata.exists())
+
+try:
+    count = 0
+    for record in uvdata.history:
+        print record
+        if record[0:5] == 'FITLD':
+            count += 1
+            pass
+        continue
+    assert(count > 5)
+
+finally:
+    uvdata.zap()
