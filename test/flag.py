063da8237137b87b8a548355640283783f4d0a9b
Mark Kettenis <kettenis@jive.eu>
2006-01-25 12:01:52 +0100

    New test.

---
+++ test/flag.py
@@ -0,0 +1,68 @@
+from AIPS import AIPS
+from AIPSTask import AIPSTask
+from AIPSData import AIPSUVData
+from Wizardry.AIPSData import AIPSUVData as WAIPSUVData
+
+import os
+import urllib
+
+AIPS.userno = 1999
+
+# Download a smallish FITS file from the EVN archive.
+url = 'http://archive.jive.nl/exp/N03L1_030225/fits/n03l1_1_1.IDI1'
+file = '/tmp/' + os.path.basename(url)
+if not os.path.isfile(file):
+    urllib.urlretrieve(url, file)
+assert(os.path.isfile(file))
+
+name = os.path.basename(url).split('_')[0].upper()
+uvdata = AIPSUVData(name, 'UVDATA', 1, 1)
+if uvdata.exists():
+    uvdata.zap()
+    pass
+
+assert(not uvdata.exists())
+
+fitld = AIPSTask('fitld')
+fitld.infile = file
+fitld.outdata = uvdata
+fitld.msgkill = 2
+fitld.go()
+
+assert(uvdata.exists())
+
+try:
+    uvflg = AIPSTask('uvflg')
+    uvflg.indata = uvdata
+    uvflg.bchan = 1
+    uvflg.echan = 1
+    uvflg.antennas[1] = 1
+    uvflg.stokes = 'CROS'
+    uvflg.reason = 'TEST'
+    uvflg()
+    
+    print uvdata.tables
+    fqtable = uvdata.table('FQ', 0)
+    print fqtable[0]
+    fgtable = uvdata.table('FG', 0)
+    for row in fgtable:
+        print row
+        continue
+    assert (fgtable[0].pflags == [0, 0, 1, 1])
+
+    uvdata2 = WAIPSUVData(uvdata.name, uvdata.klass, uvdata.disk, uvdata.seq)
+    fgtable = uvdata2.table('FG', 0)
+    row = fgtable[0]
+    row.ants = [2, 0]
+    fgtable.append(row)
+    fgtable.close()
+    
+    fgtable = uvdata.table('FG', 0)
+    for row in fgtable:
+        print row
+        continue
+    
+    assert (fgtable[1].ants == [2, 0])
+
+finally:
+    uvdata.zap()
