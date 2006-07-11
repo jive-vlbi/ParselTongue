e273877bbd6bcc6816717bd8c5c0321e33015cd1
Mark Kettenis <kettenis@jive.eu>
2006-07-11 11:36:15 +0200

    Make test more robust.

--- test/uvcon.py
+++ test/uvcon.py
@@ -2,6 +2,8 @@ from AIPS import AIPS
 from AIPSTask import AIPSTask
 from AIPSData import AIPSUVData
 
+import os
+
 AIPS.userno = 1999
 
 name = 'UVCON'
@@ -10,11 +12,13 @@ if uvdata.exists():
     uvdata.zap()
     pass
 
+os.system('cp VLBA1.UVCON /tmp')
+
 assert(not uvdata.exists())
 
 uvcon = AIPSTask('uvcon', version='31DEC06')
 uvcon.outdata = uvdata
-uvcon.infile = 'VLBA1.UVCON'
+uvcon.infile = '/tmp/VLBA1.UVCON'
 uvcon.smodel = [None, 1.0, 0.0, 0.0, 0]
 uvcon.aparm = [None, 1.4, 0, 30, -12, 12, 0, 120, 1, 1]
 uvcon.bparm = [None, -1, 0, 0, 0, 0]
