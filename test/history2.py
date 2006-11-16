4491c0456d3c7f84c1f4d52dc1206f8448f47993
Mark Kettenis <kettenis@jive.eu>
2006-11-16 10:11:51 +0100

    New test.

---
+++ test/history2.py
@@ -0,0 +1,60 @@
+from AIPS import AIPS
+from AIPSTask import AIPSTask
+from AIPSData import AIPSImage
+from Wizardry.AIPSData import AIPSImage as WizAIPSImage
+
+import os
+import urllib
+
+AIPS.userno = 1999
+
+# Download a smallish FITS file from the EVN archive.
+url = 'http://archive.jive.nl/exp/N05L1_050301/pipe/n05l1_4C39.25_ICLN.FITS'
+file = '/tmp/' + os.path.basename(url)
+if not os.path.isfile(file):
+    urllib.urlretrieve(url, file)
+assert(os.path.isfile(file))
+
+name = os.path.basename(url).split('_')[1].upper()
+image = AIPSImage(name, 'ICLN', 1, 1)
+if image.exists():
+    image.zap()
+    pass
+
+assert(not image.exists())
+
+fitld = AIPSTask('fitld')
+fitld.infile = file
+fitld.outdata = image
+#fitld.msgkill = 2
+fitld.go()
+
+assert(image.exists())
+
+try:
+    count = 0
+    for record in image.history:
+        #print record
+        if record[0:5] == 'FRING':
+            count += 1
+            pass
+        continue
+    assert(count > 5)
+
+    image = WizAIPSImage(image)
+    history = image.history
+    history.append('Something new!')
+    history.close()
+
+    image = AIPSImage(image)
+    seen = 0
+    for record in image.history:
+        #print record
+        if record[0:5] == 'Somet':
+            seen = 1
+            pass
+        continue
+    assert(seen)
+
+finally:
+    image.zap()
