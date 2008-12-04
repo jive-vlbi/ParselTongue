053017f2ae65eb7729739e940be3ad2e8302bf96
cvs-fast-export <cvs-fast-export>
2008-12-04 11:16:56 +0000

    Synthetic commit for incomplete tag parseltongue-1_1_1

--- python/Proxy/AIPSTask.py
+++ python/Proxy/AIPSTask.py
@@ -193,13 +193,8 @@ class _AIPSTaskParams:
 class AIPSTask(Task):
 
     # List of adverbs referring to file names.
-    # some new for 31DEC08
-    # O.W.  07 Nov 2008
     _file_adverbs = ['infile', 'infile2', 'outfile', 'outprint',
-                     'ofmfile', 'boxfile', 'oboxfile',
-                     'intext', 'outtext', 'datain', 'dataout', 'calin',
-                     'inlist', 'data2in'
-                     ]
+                     'ofmfile', 'boxfile', 'oboxfile']
 
     def __init__(self):
         Task.__init__(self)
