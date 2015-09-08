951c5c2fe6b68d1e380871ee897577fd590c3c80
Mark Kettenis <kettenis@jive.eu>
2015-09-08 12:02:58 +0200

    Update for ParselTongue 2.3.

--- brew/parseltongue.rb
+++ brew/parseltongue.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Parseltongue < Formula
   homepage "http://www.jive.nl/jivewiki/doku.php?id=parseltongue:parseltongue"
-  url "http://www.jive.nl/parseltongue/releases/parseltongue-2.2.tar.gz"
-  sha1 "38bc93c0057e1864af9752a641d13e501cc87209"
+  url "http://www.jive.nl/parseltongue/releases/parseltongue-2.3.tar.gz"
+  sha256 "acc335d85dad8a5996ab4eb1088f5aa28a0e67278a20c002fde43956ded0d6db"
 
   depends_on "obit"
 
